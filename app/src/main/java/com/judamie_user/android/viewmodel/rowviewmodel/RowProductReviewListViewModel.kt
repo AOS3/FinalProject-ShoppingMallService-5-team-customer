821ebd22599713fdb918824e1c227122bcb20e0b
seoyoung_31 <sy96731@naver.com>
2025-01-17 01:26:43 +0900

    리뷰 목록 화면

---
+++ app/src/main/java/com/judamie_user/android/viewmodel/rowviewmodel/RowProductReviewListViewModel.kt
@@ -0,0 +1,8 @@
+package com.judamie_user.android.viewmodel.rowviewmodel
+
+import androidx.lifecycle.ViewModel
+import com.judamie_user.android.ui.subfragment.ProductReviewListFragment
+
+data class RowProductReviewListViewModel(val productReviewListFragment: ProductReviewListFragment) : ViewModel() {
+
+}
