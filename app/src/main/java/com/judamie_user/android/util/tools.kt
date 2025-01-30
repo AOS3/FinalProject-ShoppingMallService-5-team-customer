e6d9e9ef9282e75924eb5c16ee1ce98b1a2520d0
soobu <soobum007@naver.com>
2025-01-30 17:18:31 +0900

    WishListFragment, ViewPagerFragment, ProductInfoFragment, 찜 버튼 db에 연동

--- app/src/main/java/com/judamie_user/android/util/tools.kt
+++ app/src/main/java/com/judamie_user/android/util/tools.kt
@@ -1,5 +1,8 @@
 package com.judamie_user.android.util
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
 import java.text.DecimalFormat
 
 class tools {
@@ -8,6 +11,17 @@ class tools {
         fun Int.formatToComma(): String {
             val formatter = DecimalFormat("#,###원")
             return formatter.format(this)
+        }
+    }
+
+    object BindingAdapters {
+
+        @JvmStatic
+        @BindingAdapter("bind:srcCompat")
+        fun setImageSrcCompat(imageView: ImageView, imageRes: LiveData<Int>?) {
+            imageRes?.value?.let { resId ->
+                imageView.setImageResource(resId)
+            }
         }
     }
 
